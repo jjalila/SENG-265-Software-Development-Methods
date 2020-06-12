3c4283d52f55edf878f780e0a3c97feec78b856e
jjalila <62189449+jjalila@users.noreply.github.com>
2020-06-11 21:33:41 -0700

    Create quiz_marks.py

---
+++ Assignment 3/quiz_marks.py
@@ -0,0 +1,25 @@
+import libxml2
+import sys
+import quiz_library
+
+'''
+purpose
+	Accept 1 or more log file names on the command line.
+	For each log file
+		write to standard output the course mark for the log file,
+		in CSV format
+preconditions
+	Each command-line argument is the name of a legal, readable quiz log file.
+
+	All of the log files have the same number of questions.
+'''
+
+# handle command line arguments
+if len(sys.argv) < 2:
+	print 'Syntax:', sys.argv[0], 'quiz_log_file ...'
+	sys.exit()
+
+for f in sys.argv[1:]:
+	print f+','+str(quiz_library.compute_mark_list(
+		quiz_library.load_quiz_log(f)
+		).count(1))
